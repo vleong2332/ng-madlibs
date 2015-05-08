c4c6097dbe43166619a8ff6a83ed05dfe82f63af
VICKY LEONG <VLEONG2332@GMAIL.COM>
2015-05-08 15:59:00 -0500

    Moving inits to controller

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 angular.module('madLibs', [])
-	.constant('VERSION', 1.1)
-	.controller('madLibsCtrl', function(VERSION, $scope) {
-		$scope.version = VERSION;
+	.controller('madLibsCtrl', function($scope) {
+		$scope.maleName = "Vicky Leong";
+		$scope.jobTitle = "";
+		$scope.tediousTask = "";
+		$scope.dirtyTask = "";
+		$scope.celebrity = "";
+		$scope.uselessSkill = "";
+		$scope.obnoxCeleb = "";
+		$scope.hugeNumber = "";
+		$scope.adjective = "";
 	});
