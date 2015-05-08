4403b3aa55d0e353e60aa0f970dbc63c4b9fa2df
VICKY LEONG <VLEONG2332@GMAIL.COM>
2015-05-08 16:36:38 -0500

    Added male/female name support

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 angular.module('madLibs', [])
 	.controller('madLibsCtrl', function($scope) {
-		$scope.maleName = "Vicky Leong";
+		$scope.gender = "M";
+		$scope.name = "Vicky Leong";
 		$scope.jobTitle = "";
 		$scope.tediousTask = "";
 		$scope.dirtyTask = "";
