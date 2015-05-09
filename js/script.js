59848df670c52097a693dec5aecc54045b221ae9
VICKY LEONG <VLEONG2332@GMAIL.COM>
2015-05-08 20:25:09 -0500

    Male and Female support changed to functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 angular.module('madLibs', [])
 	.controller('madLibsCtrl', function($scope) {
 		$scope.gender = "M";
+		$scope.firstPro = "he";
+		$scope.secondPro = "him";
+		$scope.possPro  = "his";
+		$scope.changeToMale = function() {
+			$scope.firstPro = "he";
+			$scope.secondPro = "him";
+			$scope.possPro  = "his";
+		};
+		$scope.changeToFemale = function() {
+			$scope.firstPro = "she";
+			$scope.secondPro = "her";
+			$scope.possPro  = "her";
+		};
 		$scope.name = "Vicky Leong";
 		$scope.jobTitle = "";
 		$scope.tediousTask = "";
