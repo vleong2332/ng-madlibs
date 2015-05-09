f241354cff51edb2105588498e949a0256e734ab
VICKY LEONG <VLEONG2332@GMAIL.COM>
2015-05-09 18:49:12 -0500

    Refractor app into two stages

--- js/script.js
+++ js/script.js
@@ -1,26 +1,58 @@
-angular.module('madLibs', [])
-	.controller('madLibsCtrl', function($scope) {
-		$scope.gender = "M";
-		$scope.firstPro = "he";
+var App = angular.module('madLibs', ['ngMessages']);
+App.controller('madLibsCtrl', function($scope) {
+	$scope.view = "story";
+
+	$scope.gender = "M";
+	$scope.firstPro  = "he";
+	$scope.secondPro = "him";
+	$scope.possPro   = "his";
+
+	$scope.changeToMale = function() {
+		$scope.firstPro  = "he";
 		$scope.secondPro = "him";
-		$scope.possPro  = "his";
-		$scope.changeToMale = function() {
-			$scope.firstPro = "he";
-			$scope.secondPro = "him";
-			$scope.possPro  = "his";
-		};
-		$scope.changeToFemale = function() {
-			$scope.firstPro = "she";
-			$scope.secondPro = "her";
-			$scope.possPro  = "her";
-		};
-		$scope.name = "Vicky Leong";
-		$scope.jobTitle = "";
-		$scope.tediousTask = "";
-		$scope.dirtyTask = "";
-		$scope.celebrity = "";
+		$scope.possPro   = "his";
+	};
+	$scope.changeToFemale = function() {
+		$scope.firstPro  = "she";
+		$scope.secondPro = "her";
+		$scope.possPro   = "her";
+	};
+
+	$scope.name         = "";
+	$scope.jobTitle     = "";
+	$scope.tediousTask  = "";
+	$scope.dirtyTask    = "";
+	$scope.celebrity    = "";
+	$scope.uselessSkill = "";
+	$scope.obnoxCeleb   = "";
+	$scope.hugeNumber   = "";
+	$scope.adjective    = "";
+
+	$scope.submitWords = function() {
+		if ($scope.madlibsWords.$valid) {
+			// console.log("form is valid");
+			$scope.view = "story";
+		}
+		else {
+			console.log('form is invalid');
+		}
+	};
+
+	$scope.startOver = function() {
+		console.log('starting over');
+		$scope.view         = "words";
+		$scope.gender       = "M"; 
+		$scope.firstPro     = "he";
+		$scope.secondPro    = "him";
+		$scope.possPro      = "his";
+		$scope.name         = "";
+		$scope.jobTitle     = "";
+		$scope.tediousTask  = "";
+		$scope.dirtyTask    = "";
+		$scope.celebrity    = "";
 		$scope.uselessSkill = "";
-		$scope.obnoxCeleb = "";
-		$scope.hugeNumber = "";
-		$scope.adjective = "";
-	});
+		$scope.obnoxCeleb   = "";
+		$scope.hugeNumber   = "";
+		$scope.adjective    = "";
+	}
+});
